20387a95517c517b5ae587fabbf0a182a2d88e29
Geoaliny <zgd_geo@yahoo.com>
2021-04-07 20:21:30 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import javax.xml.xpath.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class Main {
+
+
+    public static void main(String[] args) throws XPathExpressionException, FileNotFoundException {
+        XPathFactory factory = XPathFactory.newInstance();
+        XPath path = factory.newXPath();
+        XPathExpression xPathExpression = path.compile("dictionary");
+
+        File xmlDocument = new File("dictionary");
+        InputSource inputSource = new InputSource(new FileInputStream(xmlDocument));
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("dictionary");
+
+        Object result = xPathExpression.evaluate(inputSource, XPathConstants.NODESET);
+
+        NodeList nodeList = (NodeList) result;
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            System.out.println("Node name:" + nodeList.item(i).getNodeName());
+            System.out.println("Node value:" + nodeList.item(i).getFirstChild().getNodeValue());
+        }
+
+
+    }
+}
